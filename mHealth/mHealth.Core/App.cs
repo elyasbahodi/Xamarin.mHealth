1ec4e48693ab3ee50cf8f92164d08fd65a57470d
Elyas Bahodi <elyasbahodi@gmail.com>
2018-11-21 11:50:40 +0100

    Initialized the project and added the relevant references.

---
+++ mHealth/mHealth.Core/App.cs
@@ -0,0 +1,32 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform.IoC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mHealth.Core
+{
+    class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            CreatableTypes()
+                .EndingWith("Repository")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+                CreatableTypes()
+                    .EndingWith("Services")
+                    .AsInterfaces()
+                    .RegisterAsLazySingleton();
+
+            RegisterAppStart(new AppStart());
+        }
+
+
+    }
+}
---
+++ mHealth/mHealth.Core/AppStart.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mHealth.Core
+{
+    class AppStart : MvxNavigatingObject, IMvxAppStart
+    {
+        public void Start(object hint = null)
+        {
+
+            //Eventually check for login Credentials here. 
+            //Show the first screen here...!
+            throw new NotImplementedException();
+        }
+    }
+}
