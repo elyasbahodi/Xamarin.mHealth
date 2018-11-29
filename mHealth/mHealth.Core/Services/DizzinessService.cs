8ee01e16d61bad2036e70848571d33fc43acddf9
Elyas Bahodi <elyasbahodi@gmail.com>
2018-11-29 13:11:32 +0100

    All activity classes and folders for the services have been created.

---
+++ mHealth/mHealth.Core/Services/DizzinessService.cs
@@ -0,0 +1,24 @@
+﻿using mHealth.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mHealth.core.Services
+{
+    public class DizzinessService
+    {
+        public APIConnection APIConnection { get; set; }
+
+        public DizzinessService()
+        {
+            APIConnection = new APIConnection();
+        }
+        public bool Create(Dizziness dizziness)
+        {
+            Task<bool> task = APIConnection.PostJsonToApi("api/Dizziness/{id}?height={height}&date={date}&level={level}", dizziness);
+            return task.IsCompleted;
+        }
+    }
+}
---
+++ mHealth/mHealth.Core/Services/ExerciseService.cs
@@ -0,0 +1,27 @@
+﻿using mHealth.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mHealth.core.Services
+{
+    public class ExerciseService
+    {
+        public APIConnection APIConnection { get; set; }
+
+        public ExerciseService()
+        {
+            APIConnection = new APIConnection();
+        }
+        public bool Create(Exercise exercise)
+        {
+            Task<bool> task = APIConnection.PostJsonToApi("api/Exercise?title={title}&description={description}", exercise);
+            return task.IsCompleted;
+
+
+        }
+
+    }
+}
