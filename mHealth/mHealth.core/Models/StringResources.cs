acc6a997ac223a15c543cb1c652bfe51a233ad65
kenn84 <kenneth84petersen@hotmail.com>
2018-11-30 10:07:22 +0100

    StringResources created

---
+++ mHealth/mHealth.core/Models/StringResources.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mHealth.core.Models
+{
+    public class StringResources
+    {
+        
+       
+    
+        public Dictionary<string, string> Login()
+        {
+            Dictionary<string, string> LoginStrings = new Dictionary<string, string>()
+            {
+                {"@Heading", "Her kan du logge ind" },
+                {"@lblCpr", "Cpr : " },
+                {"@lblKodeord" , "Kodeord : "},
+                {"@txtLogin", "Login" },
+                {"@lblProfil", "Har du ingen konto ?" },
+                {"@txtOpretProfil", "Opret profil" }
+
+            };
+            return LoginStrings; 
+        }
+     
+
+
+
+
+
+
+
+
+    }
+}
